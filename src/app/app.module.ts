407965d3e63b44d97801bea0e608b261bd482743
Mike Munene <mike.munene@moringaschool.com>
2022-01-18 18:44:50 +0300

    Create a component which handles the view of the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewsComponent } from './views/views.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewsComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "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"
 };
 
 /*
